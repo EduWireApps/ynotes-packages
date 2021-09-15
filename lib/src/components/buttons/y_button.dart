abd8f130fa24b765432e52b2c21fbbb6f87de7bc
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-09-15 16:09:32 +0200

    document button

--- lib/src/components/buttons/y_button.dart
+++ lib/src/components/buttons/y_button.dart
@@ -1,32 +1,72 @@
 part of components;
 
+/// The variant of the [YButton] component.
 enum YButtonVariant { contained, outlined, text }
+
+/// The [YButton] size.
 enum YButtonSize { medium, large }
 
-// Stateful to use TickerProviderStateMixin
+/// A button component.
 class YButton extends StatefulWidget {
+  /// The action to perform when the button is pressed.
   final VoidCallback onPressed;
-  final VoidCallback? onPressedDisabled;
-  final VoidCallback? onPressedLoading;
+
+  /// The action to perform when the button is long pressed.
   final VoidCallback? onLongPress;
+
+  /// The action to perform when the button is loading. If not provided,
+  /// the button will be disabled.
+  ///
+  /// Can be used to inform the user must wait for an action to complete.
+  final VoidCallback? onPressedLoading;
+
+  /// The action to perform when the button is disabled. If not provided,
+  /// the button will be disabled.
+  ///
+  /// Can be used to inform the user what are the reasons that make the button
+  /// disabled.
+  final VoidCallback? onPressedDisabled;
+
+  /// The text to display on the button. Must be short.
   final String text;
+
+  /// The color of the button, [YColor.primary] by default.
   final YColor color;
+
+  /// The variant of the button, [YButtonVariant.contained] by default.
   final YButtonVariant variant;
+
+  /// The size of the button, [YButtonSize.medium] by default.
   final YButtonSize size;
+
+  /// The icon to display on the button, next to the [text].
   final IconData? icon;
+
+  /// The condition to set the button's state to loading.
   final bool isLoading;
+
+  /// The condition to set the button's state to disabled.
   final bool isDisabled;
+
+  /// The condition to show the icon to the right of the text.
   final bool isIconReversed;
+
+  /// The condition to make the button full width.
+  ///
+  /// Under the hood, we wrap the button with a [Row] and an [Expanded] widgets.
   final bool block;
+
+  /// The condition to make the button rounded ([YBorderRadius.full]).
   final bool rounded;
 
+  /// A button component.
   const YButton(
       {Key? key,
       required this.onPressed,
       this.onPressedDisabled,
       this.onPressedLoading,
-      required this.text,
       this.onLongPress,
+      required this.text,
       this.color = YColor.primary,
       this.variant = YButtonVariant.contained,
       this.size = YButtonSize.medium,

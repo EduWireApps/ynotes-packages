2453b129b71b671ff8f75d62e0078c47ac46894a
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-09-18 14:42:51 +0200

    document slider

--- lib/src/components/inputs/y_slider.dart
+++ lib/src/components/inputs/y_slider.dart
@@ -1,14 +1,37 @@
 part of components;
 
+/// A slider component.
 class YSlider extends StatelessWidget {
+  /// The slider's value.
   final double value;
+
+  /// Called during a drag when the user is selecting a new value for the slider by dragging.
+  ///
+  /// The slider passes the new value to the callback but does not actually change state until the parent widget rebuilds the slider with the new value.
+  ///
+  /// If null, the slider will be displayed as disabled.
   final ValueChanged<double> onChanged;
+
+  /// The slider's minimum value.
   final double min;
+
+  /// The slider's maximum value.
   final double max;
+
+  /// The number of discrete divisions.
+  ///
+  /// Typically used with [label] to show the current discrete value.
+  ///
+  /// If null, the slider is continuous.
   final int? divisions;
+
+  /// The slider's label. Defaults to [value].
   final String? label;
+
+  /// The slider's color. Defaults to [YColor.primary].
   final YColor color;
 
+  /// A slider component.
   const YSlider(
       {Key? key,
       required this.value,
@@ -20,14 +43,14 @@ class YSlider extends StatelessWidget {
       this.color = YColor.primary})
       : super(key: key);
 
-  YTColor get style => theme.colors.get(this.color);
+  YTColor get _style => theme.colors.get(this.color);
 
   @override
   Widget build(BuildContext context) {
     return SliderTheme(
       data: SliderThemeData(
-          valueIndicatorColor: style.backgroundColor,
-          valueIndicatorTextStyle: theme.texts.body1.copyWith(color: style.foregroundColor)),
+          valueIndicatorColor: _style.backgroundColor,
+          valueIndicatorTextStyle: theme.texts.body1.copyWith(color: _style.foregroundColor)),
       child: Slider(
           value: this.value,
           onChanged: this.onChanged,
@@ -35,8 +58,8 @@ class YSlider extends StatelessWidget {
           max: this.max,
           label: this.label ?? this.value.round().toString(),
           divisions: this.divisions,
-          inactiveColor: style.lightColor,
-          activeColor: style.backgroundColor),
+          inactiveColor: _style.lightColor,
+          activeColor: _style.backgroundColor),
     );
   }
 }

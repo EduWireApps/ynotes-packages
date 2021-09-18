e1935ebed6a99d0d4fac52764db96fa9b24955ce
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-09-18 15:20:34 +0200

    document form field

--- lib/src/components/inputs/form/y_form_field.dart
+++ lib/src/components/inputs/form/y_form_field.dart
@@ -1,23 +1,68 @@
 part of components;
 
-enum YFormFieldInputType { text, password, email, number, phone, url, datetime }
+// TODO: implement behavior depending on [YFormFieldInputType]
 
+// The different types of input fields
+enum YFormFieldInputType { text, password, email, number, phone, url, datetime, options }
+
+/// An input field that can be used in a [YForm].
 class YFormField extends StatefulWidget {
+  /// The type of the input field.
+  ///
+  /// **WARNING:** It's type will change for [YFormFieldInputType] in the future.
   final TextInputType type;
+
+  /// The default of value of the input.
   final String? defaultValue;
+
+  /// The function that will be called when the value of the input changes.
   final ValueChanged<String>? onChanged;
+
+  /// The condition to allow autocorrect.
   final bool autocorrect;
+
+  /// The text capitalization of the input.
   final TextCapitalization textCapitalization;
+
+  /// The max length of the input. It will be shown at its bottom right.
   final int? maxLength;
+
+  /// Condition to make the input expandable, like a `textearea` in web development.
   final bool expandable;
+
+  /// The validator of the input. This function is run when the form containing
+  /// this input field is validated.
+  ///
+  /// The function will receive the value of the input and must return a
+  /// [String] (if the value is invalid ans that will be shown as the error)
+  /// or [null] (if the value is valid and the error will not be shown).
+  ///
+  /// If the function is not provided, the field is always considered valid.
   final String? Function(String?)? validator;
+
+  /// The condition to disable the input.
   final bool disabled;
+
+  /// The label of the input.
   final String label;
+
+  /// The placeholder of the input.
   final String? placeholder;
+
+  /// The helper text of the input, used to help the user.
   final String? helper;
+
+  /// The mutable properties of the input. **Will be overriden when used in a [YForm]**.
+  /// ```dart
+  /// properties: YFormFieldProperties();
+  /// ```
   final YFormFieldProperties properties;
+
+  /// The function that will be called when the form is saved. Useful to save all
+  /// the values of the inputs of a form.
   final ValueChanged<String?> onSaved;
 
+  /// An input field that can be used in a [YForm].
   const YFormField(
       {Key? key,
       required this.type,
@@ -88,6 +133,7 @@ class _YFormFieldState extends State<YFormField> {
         validator: widget.validator != null ? this.validate : null,
         onEditingComplete: widget.properties.onEditingComplete,
         onSaved: widget.onSaved,
+        readOnly: widget.disabled,
         decoration: InputDecoration(
             filled: true,
             fillColor: theme.colors.backgroundLightColor,

77a1f7bf7d6e42acf61d5bb7851c980f2f4d509f
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-09-18 15:06:45 +0200

    document radio

--- lib/src/components/inputs/radio/y_radio.dart
+++ lib/src/components/inputs/radio/y_radio.dart
@@ -1,16 +1,26 @@
 part of components;
 
+/// A radio input.
 class YRadio<T> extends StatelessWidget {
+  /// The value of the radio.
   final T value;
+
+  /// The currently selected value for a group of radio buttons.
   final T groupValue;
+
+  /// The function that gets triggered when the radio is selected. The value obtained
+  /// must be used to update the state of the radio's [groupValue].
   final ValueChanged<dynamic> onChanged;
+
+  /// The color of the radio. Defaults to [YColor.primary].
   final YColor color;
 
+  /// A radio input.
   const YRadio(
       {Key? key, required this.value, required this.groupValue, required this.onChanged, this.color = YColor.primary})
       : super(key: key);
 
-  YTColor get style => theme.colors.get(this.color);
+  YTColor get _style => theme.colors.get(this.color);
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +30,7 @@ class YRadio<T> extends StatelessWidget {
         value: this.value,
         groupValue: this.groupValue,
         onChanged: this.onChanged,
-        activeColor: style.backgroundColor,
+        activeColor: _style.backgroundColor,
         // onChanged: _onChanged,
       ),
     );
--- lib/src/components/inputs/radio/y_radio_list_tile.dart
+++ lib/src/components/inputs/radio/y_radio_list_tile.dart
@@ -1,13 +1,27 @@
 part of components;
 
+/// A list tile that uses [YRadio].
 class YRadioListTile<T> extends StatelessWidget {
+  /// The title of the tile.
   final String title;
+
+  /// The radio value.
   final T value;
+
+  /// The currently selected value for a group of radio buttons.
   final T groupValue;
+
+  /// The function that gets triggered when the radio is selected. The value obtained
+  /// must be used to update the state of the radio's [groupValue].
   final ValueChanged<T> onChanged;
+
+  /// The color of the radio. Defaults to [YColor.primary].
   final YColor color;
+
+  /// The condition to display the radio at the end of the tile instead.
   final bool end;
 
+  /// A list tile that uses [YRadio].
   const YRadioListTile(
       {Key? key,
       required this.title,
